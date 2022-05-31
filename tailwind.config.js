d3f9e4f7c8c0c24756769dfd07dd1ce7d6fa32f1
Westindiess <gregoryalexiss@gmail.com>
2022-06-01 00:07:47 +0200

    tailwins css classes

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      textColor: {
+        primary: "#6439ff",
+      },
+      flex: {
+        6: "6 6 0%",
+      },
+      boxShadow: {
+        search: "0px 1px 0px 0.5px rgb(0 0 0 / 0.2)",
+      },
+    },
+  },
+  plugins: [],
+};
