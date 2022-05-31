dffeda4eaeb4dafdf063a1c8075b3a2f85d2d619
Westindiess <gregoryalexiss@gmail.com>
2022-06-01 00:09:22 +0200

    add Application routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import New from "./Pages/New/New";
+import Single from "./Pages/Single/Single";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" />
+      <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Route path="users">
+        <Route index element={<List />} />
+        <Route path=":userId" element={<Single />} />
+        <Route path="new" element={<New />} />
+      </Route>
+
+      <Route path="products">
+        <Route index element={<List />} />
+        <Route path=":productId" element={<Single />} />
+        <Route path="new" element={<New />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
