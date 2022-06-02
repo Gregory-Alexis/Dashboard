d1f9d1d1028a6154b246a1be768dabf2240ae8d2
Westindiess <gregoryalexiss@gmail.com>
2022-06-02 23:34:29 +0200

    new class css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,19 @@ module.exports = {
       textColor: {
         primary: "#6439ff",
       },
+      backgroundColor: {
+        primary: "#6439ff",
+      },
       flex: {
+        2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
+        5: "5 5 0%",
         6: "6 6 0%",
       },
       boxShadow: {
-        search: "0px 1px 0px 0.5px rgb(0 0 0 / 0.2)",
+        search: "0px 1px 3px 0.5px rgb(0 0 0 / 0.2)",
+        card: "4px 4px 10px 1px rgb(0 0 0 / 0.2)",
       },
     },
   },
