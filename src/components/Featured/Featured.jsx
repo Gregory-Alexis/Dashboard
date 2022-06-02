74c49a5a971d82066fa413cd21edb63560f8bd47
Westindiess <gregoryalexiss@gmail.com>
2022-06-03 01:20:02 +0200

    featured css refactored

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -8,17 +8,17 @@ import "react-circular-progressbar/dist/styles.css";
 
 const Featured = () => {
   return (
-    <div className="flex-2 justify-between p-2 shadow-card rounded-md ">
-      <div className="flex items-center justify-between text-gray-400">
-        <h1 className="text-bas font-semibold">Total Revenue</h1>
-        <MoreVert fontSize="small" />
+    <div className="flex-2 justify-between p-3 shadow-card rounded-md ">
+      <div className="flex items-center justify-between text-gray-500">
+        <h1 className="text-lg font-semibold">Total Revenue</h1>
+        <MoreVert fontSize="medium" />
       </div>
-      <div className="p-5 flex flex-col items-center justify-center gap-2">
-        <div className="w-28 h-28">
+      <div className="pt-8 flex flex-col items-center justify-center gap-6">
+        <div className="w-40 h-40">
           <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
         <p className="font-semibold text-gray-400">Total sales made today</p>
-        <p className="text-lg">$420</p>
+        <p className="text-5xl">$420</p>
         <p className="font-light text-base text-gray-600 text-center">
           Previous transaction processing. Last payments may not be included
         </p>
