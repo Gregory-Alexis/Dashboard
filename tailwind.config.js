4012ff3ed83e7619d3b25f646489fe04ce93f7d9
Westindiess <gregoryalexiss@gmail.com>
2022-06-06 19:05:41 +0200

    edit box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
       },
       backgroundColor: {
         primary: "#6439ff",
+        approved: "rgba(0, 128, 0, 0.151)",
+        pending: "rgba(189, 189, 3, 0.200)",
       },
       flex: {
         2: "2 2 0%",
@@ -17,7 +19,8 @@ module.exports = {
       },
       boxShadow: {
         search: "0px 1px 3px 0.5px rgb(0 0 0 / 0.2)",
-        card: "4px 4px 10px 1px rgb(0 0 0 / 0.2)",
+        card: "1px 2px 4px 1px rgb(0 0 0 / 0.2)",
+        chart: "2px 2px 4px 1px rgb(0 0 0 / 0.2)",
       },
     },
   },
