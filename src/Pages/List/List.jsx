915cce2fe03c7bbc1426ace4a6d2c44fee175b61
Westindiess <gregoryalexiss@gmail.com>
2022-06-09 12:55:37 +0200

    List component

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,5 +1,17 @@
+import Datatable from "../../components/Datatable/Datatable";
+import Navbar from "../../components/Navbar/Navbar";
+import SideBar from "../../components/Sidebar/SideBar";
+
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className="flex-6">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
 };
 
 export default List;
