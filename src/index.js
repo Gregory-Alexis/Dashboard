3812aa00b7791a003a13a91b8471251b12c0f4cd
Westindiess <gregoryalexiss@gmail.com>
2022-06-12 17:51:15 +0200

    index refactored

--- src/index.js
+++ src/index.js
@@ -1,32 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Login from "./Pages/Login/Login";
-import List from "./Pages/List/List";
-import New from "./Pages/New/New";
-import Single from "./Pages/Single/Single";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" />
-      <Route index element={<Home />} />
-      <Route path="login" element={<Login />} />
-
-      <Route path="users">
-        <Route index element={<List />} />
-        <Route path=":userId" element={<Single />} />
-        <Route path="new" element={<New />} />
-      </Route>
-
-      <Route path="products">
-        <Route index element={<List />} />
-        <Route path=":productId" element={<Single />} />
-        <Route path="new" element={<New />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+root.render(<App />);
