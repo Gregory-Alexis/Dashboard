5027de77a76b02f67a33ebc92e1ded5dc5e18d5f
Westindiess <gregoryalexiss@gmail.com>
2022-06-23 20:31:39 +0200

    user model

---
+++ server/models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, required: [true, "Please add your firstname"] },
+    lastName: { type: String, required: [true, "Please add your lastname"] },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+    password: { type: String, required: [true, "Please add a password"] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
