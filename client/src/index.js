891985c7cdbd98a9fe71069825d4db1c6b30dc66
Westindiess <gregoryalexiss@gmail.com>
2022-07-02 15:25:19 +0200

    add redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/app/store";
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer />
+  </Provider>
+);
